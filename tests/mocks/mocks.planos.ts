7d681d54a5a508d14146aee11967326aaedf83ca
Eduardo Simões <eduardorsimoes97@gmail.com>
2023-05-22 14:50:12 -0300

    :test_tube: test: Testes para tabela de planos na camada Service

---
+++ tests/mocks/mocks.planos.ts
@@ -0,0 +1,55 @@
+export const lista = [
+    {
+      id: 1,
+      nome: 'Grátis',
+      periodo: 1,
+      preco: 0,
+      createdAt: '2023-05-21T00:24:27.000Z',
+    },
+    {
+      id: 2,
+      nome: 'Básico',
+      periodo: 12,
+      preco: 1000,
+      createdAt: '2023-05-21T00:24:27.000Z',
+    },
+    {
+      id: 3,
+      nome: 'Plus',
+      periodo: 16,
+      preco: 15000,
+      createdAt: '2023-05-21T19:01:30.000Z',
+    },
+  ];
+
+export const real = [
+  {
+    id: 1,
+    nome: 'Grátis',
+    periodo: 1,
+    preco: 0,
+    createdAt: '2023-05-21T00:24:27.000Z',
+  },
+  {
+    id: 2,
+    nome: 'Básico',
+    periodo: 12,
+    preco: 10,
+    createdAt: '2023-05-21T00:24:27.000Z',
+  },
+  {
+    id: 3,
+    nome: 'Plus',
+    periodo: 16,
+    preco: 150,
+    createdAt: '2023-05-21T19:01:30.000Z',
+  },
+];
+
+export const create = { id: 1 };
+
+export const del = [1];
+
+export const up = [1];
+
+export const planoData = { nome: 'Premium', periodo: 16, preco: 150 };
