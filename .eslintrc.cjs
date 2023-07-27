05f5870fc418493ad5c9d4c8ff53c74b00545c07
FJackson <jacksonfinal@gmail.com>
2023-07-27 20:12:34 -0300

    preparando codigo para eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+        'indent': ['error', 4],
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'double'],
+        'space-infix-ops': 'error',
+        'keyword-spacing': 'error',
+        'space-before-blocks': 'error',
+        'space-before-function-paren': ['error', 'never'],
+        'max-len': ['error', { 'code': 120, 'tabWidth': 4 }],
+        'object-curly-spacing': ['error', 'always'],
+        'func-call-spacing': 'error',
+        'no-mixed-spaces-and-tabs': 'error',
+        'eqeqeq': 'off',
+        '@typescript-eslint/no-explicit-any': 'on',
+        '@typescript-eslint/require-await': 'on',
+    },
+};
