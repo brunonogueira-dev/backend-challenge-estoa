6f1658c338bd182a6dcf169a8383750a58b4ffa0
FJackson <jacksonfinal@gmail.com>
2023-07-27 12:04:47 -0300

    refatorando database connection

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -6,18 +6,10 @@ const dbUser = process.env.DB_USER || "";
 const dbHost = process.env.DB_HOST || "";
 const dbPassword = process.env.DB_PASSWORD || "";
 
-console.log("ENV", dbName, dbUser, dbPassword, dbHost)
-
 const db = new Sequelize(dbName, dbUser, dbPassword, {
     dialect: "mysql",
     host: dbHost,
     port: 3306
-});
-
-db.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-}).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
 });
 
 export default db;
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import bodyParser from 'body-parser'
 
 import { Router } from 'express';
 import db from './configs/db';
-import User from './models/user';
 import Plan from './models/plan';
 import Signature from './models/signature';
+import User from './models/user';
 
 const app = express();
 
@@ -19,30 +19,41 @@ export const route = Router();
 
 app.use(route);
 
+const createFreePlan = async () => {
+  const planExists = await Plan.findOne({where: {name: 'free'}});
+  if (!planExists) {
+    await Plan.create({
+      name: 'free',
+      price: 0,
+      expiration: 1
+    })
+  }
+}
+
+const syncDb = async () => {
+  await db.authenticate().then(async () => {
+      await db.sync();
+      await User.sync();
+      await Plan.sync();
+      await Signature.sync();
+      await createFreePlan();
+
+      console.log("Database successfully connected");
+  }).catch((error) => {
+      throw new Error(error);
+  });
+}
+
 async function startServer() {
   try {
-    await db.sync();
-    await User.sync();
-    await Plan.sync();
-    await Signature.sync();
+    await syncDb();
 
-    const planExists = await Plan.findOne({where: {name: 'free'}});
-    if (!planExists) {
-      await Plan.create({
-        name: 'free',
-        price: 0,
-        expiration: 1
-      })
-    }
-
-    console.log("Database successfully connected");
+    app.listen(3333, () => {
+      console.log('Server running on port 3333');
+    });
   } catch (err) {
     console.log("Error", err);
   }
-
-  app.listen(3333, () => {
-    console.log('Server running on port 3333');
-  });
 }
 
 startServer();
