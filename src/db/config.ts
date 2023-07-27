a370056e9973d56d43cc7a48f8d4c39afc5b0f6e
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-07-27 19:16:16 -0300

    Acrescentados valores default se nao houver .env

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,24 +1,15 @@
 import { Dialect, Sequelize } from "sequelize";
 require("dotenv").config();
 
-const dbName = process.env.DB_NAME as string;
-const dbUser = process.env.DB_USER as string;
-const dbHost = process.env.DB_HOST;
-const dbDriver = process.env.DB_DRIVER as Dialect;
-const dbPassword = process.env.DB_PASSWORD;
+const dbName = (process.env.DB_NAME as string) || "root";
+const dbUser = (process.env.DB_USER as string) || "password";
+const dbHost = process.env.DB_HOST || "localhost";
+const dbDriver = (process.env.DB_DRIVER as Dialect) || "mysql";
+const dbPassword = process.env.DB_PASSWORD || "password";
 
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
     dialect: dbDriver,
 });
-
-sequelizeConnection
-    .authenticate()
-    .then(() => {
-        console.log("Connection has been established successfully.");
-    })
-    .catch((error) => {
-        console.error("Unable to connect to the database: ", error);
-    });
 
 export default sequelizeConnection;
