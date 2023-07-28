8fd345f3c4abde3349b4ceeacf1f3c3befbc0913
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-07-28 01:24:46 -0300

    Criacao dos controllers

---
+++ src/api/controllers/plan.controller.ts
@@ -0,0 +1,76 @@
+import { Request, Response, NextFunction } from "express";
+import PlanService from "../services/plan.service";
+import { IPlan, IPlanInput } from "../interfaces";
+
+export default class PlanController {
+    private planService: PlanService;
+
+    constructor() {
+        this.planService = new PlanService();
+    }
+
+    public listAllPlans = async (
+        _: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const plans: IPlan[] = await this.planService.listAllPlans();
+        res.json(plans);
+    };
+
+    public findPlanById = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const { id } = req.params;
+        const plan: IPlan | null = await this.planService.findPlanById(id);
+        if (!plan) {
+            res.status(404).json({ message: "Plan not found" });
+        } else {
+            res.json(plan);
+        }
+    };
+
+    public createPlan = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const input: IPlanInput = req.body;
+        const id: string = await this.planService.createPlan(input);
+        res.json({ id });
+    };
+
+    public updatePlan = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const { id } = req.params;
+        const input: IPlanInput = req.body;
+        const message: string | null = await this.planService.updatePlan(
+            id,
+            input
+        );
+        if (!message) {
+            res.status(404).json({ message: "Plan not found" });
+        } else {
+            res.json({ message });
+        }
+    };
+
+    public deletePlan = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const { id } = req.params;
+        const result = await this.planService.deletePlan(id);
+        if (!result) {
+            res.status(404).json({ message: "Plan not found" });
+        } else {
+            res.json({ message: result });
+        }
+    };
+}
