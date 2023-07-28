0d7393dd112b795f3cf46b8e21c2f3b6abf82426
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-07-28 06:14:47 -0300

    Usando JSON para o swagger

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,34 @@
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { userRouter, planRouter } from "./api/routes";
 import { errorHandler } from "./api/middlewares/errors.middleware";
-require("dotenv").config();
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
-export const get = () => {
-    const app: Application = express();
+const app = express();
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    app.get("/", async (req: Request, res: Response): Promise<Response> => {
-        return res.status(200).send({
-            message: "Welcome to the users and plans API!",
-        });
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({
+        message: "Welcome to the users and plans API!",
     });
+});
 
-    app.use("/api", userRouter);
-    app.use("/api", planRouter);
-    app.use(errorHandler);
+app.use("/api", userRouter);
+app.use("/api", planRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(errorHandler);
 
-    return app;
-};
-
-export const start = () => {
-    try {
-        const app = get();
-        app.listen(port, () => {
-            console.log(`Server running on http://localhost:${port}`);
-        });
-    } catch (error: any) {
-        console.log(`Error occurred: ${error.message}`);
-    }
-};
-
-start();
+try {
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+        console.log(`Docs can be found at http://localhost:${port}/docs`);
+    });
+} catch (error: any) {
+    console.log(`Error occurred: ${error.message}`);
+}
