0121fd85c104fc380c01690a1449ce8374fb635f
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-07-28 03:50:21 -0300

    Removida a funcao de init, usando sequelize-cli

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import { userRouter, planRouter } from "./api/routes";
 import { errorHandler } from "./api/middlewares/errors.middleware";
-import sequelizeConnection from "./db/config/config";
-import mysql2 from "mysql2";
 require("dotenv").config();
-
-const isDev = process.env.NODE_ENV === "dev";
-
-async function dbInit() {
-    try {
-        const connection = mysql2.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-        });
-        connection.connect();
-
-        const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`;
-        await connection.promise().query(createDatabaseQuery);
-
-        console.log("Database created successfully.");
-
-        connection.end();
-
-        sequelizeConnection
-            .authenticate()
-            .then(() => {
-                console.log("Connection has been established successfully.");
-            })
-            .catch((error) => {
-                console.error("Unable to connect to the database: ", error);
-            });
-
-        await sequelizeConnection.sync({ alter: isDev });
-        console.log("Connection synced succesfully.");
-    } catch (error) {
-        console.error("Error initializing database:", error);
-    }
-}
 
 require("dotenv").config();
 
@@ -62,10 +26,8 @@ export const get = () => {
     return app;
 };
 
-export const start = async () => {
+export const start = () => {
     try {
-        await dbInit();
-
         const app = get();
         app.listen(port, () => {
             console.log(`Server running on http://localhost:${port}`);
