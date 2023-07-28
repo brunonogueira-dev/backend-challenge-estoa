efac0086e9bf8715e6e0b692d77e6834127ee226
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-07-28 01:23:20 -0300

    Refatorando o config e a conexao com o db

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -1,15 +1,16 @@
 import { Dialect, Sequelize } from "sequelize";
 require("dotenv").config();
 
-const dbName = (process.env.DB_NAME as string) || "root";
-const dbUser = (process.env.DB_USER as string) || "password";
-const dbHost = process.env.DB_HOST || "localhost";
+const dbName = (process.env.DB_NAME as string) || "desafio";
+const dbUser = (process.env.DB_USERNAME as string) || "root";
+const dbHost = (process.env.DB_HOST as string) || "localhost";
 const dbDriver = (process.env.DB_DRIVER as Dialect) || "mysql";
-const dbPassword = process.env.DB_PASSWORD || "password";
+const dbPassword = (process.env.DB_PASSWORD as string) || "password";
 
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
     dialect: dbDriver,
+    logging: false,
 });
 
 export default sequelizeConnection;
