1e89d5f0f6feb73c5d5b23584225fb30d64aff82
BrunoSDomingues <brunao.signorelli@hotmail.com>
2023-07-28 03:52:57 -0300

    Usando nova config do sequelize

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -1,16 +1,13 @@
-import { Dialect, Sequelize } from "sequelize";
+import { Options } from "sequelize";
 require("dotenv").config();
 
-const dbName = (process.env.DB_NAME as string) || "desafio";
-const dbUser = (process.env.DB_USERNAME as string) || "root";
-const dbHost = (process.env.DB_HOST as string) || "localhost";
-const dbDriver = (process.env.DB_DRIVER as Dialect) || "mysql";
-const dbPassword = (process.env.DB_PASSWORD as string) || "password";
-
-const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
-    host: dbHost,
-    dialect: dbDriver,
+const config: Options = {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "password",
+    database: process.env.DB_NAME || "desafio",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: "mysql",
     logging: false,
-});
-
-export default sequelizeConnection;
+};
+export = config;
